cd6c1ba4e259e10e275e08b07b0f90c8b4041c11
Kayode <kayodefredrickbalogun@gmail.com>
2024-06-26 16:31:57 +0100

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Balogun Kayode on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
